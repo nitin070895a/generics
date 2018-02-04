c0fb6762e79afb133062b6d96ef8f51856c66ea9
nitin070895a <nitin070895@gmail.com>
2018-02-04 19:46:39 +0530

    Splash Screen Activity and Full Screen Activity added

---
+++ generics/src/main/java/nitin/thecrazyprogrammer/generics/Activities/FullScreenActivity.java
@@ -0,0 +1,22 @@
+package nitin.thecrazyprogrammer.generics.Activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * Created by Nitin Khurana on 2/4/2018.
+ */
+public abstract class FullScreenActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        super.onCreate(savedInstanceState);
+    }
+}
